ee6ce0e2a13228484fcd0c167eae44eb93428e0f
Ashubh01 <shubhamdhokare01@gmail.com>
2024-08-02 20:33:44 +0530

    Fixed env vars problem

--- frontend/src/components/pages/Auth.jsx
+++ frontend/src/components/pages/Auth.jsx
@@ -1,15 +1,27 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import queriousLogo from '../../assets/images/queriousLogo.png';
+import { backendUrl } from '../../config.js'
 
 export default function AuthComponent() {
+    const [isEmailSent, setIsEmailSent] = useState(false);
+    const [userEmail, setUserEmail] = useState('');
+    
     return (
-        <div className='bg-[#FAE593] h-screen'>
+        <div className='bg-[#2887CC] h-screen'>
             <div className='flex justify-center'>
                 <div><Heading /></div>
             </div>
-            <div className='md:max-w-[500px] md:p-[15px] md:border-[2px] md:rounded-[5px]
-                    max-w-[350px] p-[5px] bg-[#FFF6D9] border-black border-[1px] rounded-[5px]'>
-                <div><EmailInput /></div>
-                <div><CodeInput /></div>
+            <div className='my-[10px] flex justify-center'>
+                <div className='md:p-[5px] md:border-[3px] md:rounded-[10px]
+                        align-center p-[5px] border-white border-[1px] rounded-[6px]'>
+                    {!isEmailSent &&
+                        <div><EmailInput email={userEmail} setEmail={setUserEmail} setIsEmailSent={setIsEmailSent} /></div>
+                    }
+                    {isEmailSent &&
+                        <div><CodeInput  email={userEmail}/></div>
+                    }
+                </div>
             </div>
         </div>
     )
@@ -17,30 +29,48 @@ export default function AuthComponent() {
 
 const Heading = () => {
     return <div>
-        <img className='h-[320px] md:h-[380px]' src={queriousLogo} alt="Querious Logo" />
+        <img className='h-[250px] md:h-[450px]' src={queriousLogo} alt="Querious Logo" />
     </div>
 }
 
-const EmailInput = () => {
+const EmailInput = ({email, setEmail, setIsEmailSent}) => {
 
     function handleEnterEvent (event) {
         if (event.key === 'Enter') sendEmail();
     }
 
-    async function sendEmail () {
+    const sendEmail = async () => {
+        const response = await axios({
+            url: `${backendUrl}/users/send`,
+            method: 'POST',
+            headers: {
+                'Content-Type': "application/json"
+            },
+            data: {
+                'email': email
+            }
 
+        })
+        
+        console.log(response);
+        if(response.status === 200){
+            alert('Email Sent');
+            setIsEmailSent(true);
+        }
     }
 
-    return <div className='flex justify-center gap-[5px] md:gap-[20px]'>
+    return <div className='md:gap-[20px]
+                flex justify-center gap-[5px]'>
         <div>
-            <input className='md:pl-[10px] md:w-[360px] md:text-[18px]
-                p-[2px] pl-[5px] w-[250px] h-[38px] border-black border-[2px] text-[15px] rounded-[5px]'
+            <input className='md:py-[10px] md:pl-[10px] md:w-[360px] md:text-[18px]
+                p-[4px] pl-[5px] w-[220px] border-black border-[2px] text-[15px] rounded-[5px]'
                 placeholder='Enter your email' type='email' onKeyDown={handleEnterEvent}
+                onChange={(event) => { setEmail(event.target.value) }}
             />
         </div>
         <div>
-            <button onClick={sendEmail} className='md:p-[2px] md:w-[100px] md:text-[20px] md:border-[2px] md:font-semibold
-                p-[2px] w-[80px] bg-[#FED688] text-[20px] border-black border-[2px] font-semibold rounded-[7px]'>
+            <button onClick={sendEmail} className='md:py-[8px] md:px-[20px] md:text-[20px] md:rounded-[8px] md:border-[2px] md:font-semibold
+                p-[2px] px-[20px] bg-green-400 text-[18px] text-white border-black border-[2px] font-semibold rounded-[5px]'>
                 Send
             </button>
         </div>
@@ -53,8 +83,71 @@ const SendingMailLoading = () => {
     </div>
 }
 
-const CodeInput = () => {
-    return <div>
+const CodeInput = ({email}) => {
+    const [userCode, setUserCode] = useState('');
 
+    function handleEnterEvent (event) {
+        if (event.key === 'Enter') verifyCode();
+    }
+
+    async function resendEmail () {
+        const response = await axios({
+            url: `${backendUrl}/users/send`,
+            method: 'POST',
+            headers: {
+                'Content-Type': "application/json"
+            },
+            data: {
+                'email': email
+            }
+            
+        })
+        console.log(response);
+        if(response.status === 200){
+            alert('Email Sent');
+        }
+    }
+
+    async function verifyCode () {
+        const response = await axios({
+            url: `${backendUrl}/users/verify`,
+            method: 'POST',
+            headers: {
+                'Content-Type': "application/json"
+            },
+            data: {
+                'email': email,
+                'code': userCode 
+            }
+        })
+
+        console.log(response);
+        if(response.status === 200) alert('Code Valid')
+        else alert('Code Invalid')
+    }
+
+    return <div>
+        <div className='flex justify-center'>
+            <div>
+                <input className='md:p-[8px] md:w-[350px] md:pl-[10px] md:text-[18px]
+                    p-[5px] pl-[5px] text-[15px] w-[300px] rounded-[5px] border-black border-[2px]' type='text' placeholder='Enter your code'
+                    onChange={(event) => {setUserCode(event.target.value)}} onKeyDown={handleEnterEvent}
+                />
+            </div>    
+        </div>
+        <div className='flex justify-between py-[5px]'>
+            <div><button className='md:p-[10px] md:px-[45px] md:text-[23px] md:font-bold md:tracking-wide md:border-[3px] md:rounded-[10px]
+                    p-[5px] px-[44px] bg-green-400 text-[18px] text-white font-semibold tracking-tight border-black border-[2px] rounded-[5px]'
+                    onClick={resendEmail}>
+                Resend
+                </button>
+            </div>
+            <div><button className='md:p-[10px] md:px-[45px] md:text-[23px] md:font-bold md:tracking-wide md:border-[3px] md:rounded-[10px]
+                    p-[5px] px-[44px] bg-blue-400 text-[18px] text-white font-semibold tracking-tight border-black border-[2px] rounded-[5px]'
+                    onClick={verifyCode}>
+                Verify
+                </button>
+            </div>
+        </div>
     </div>
 }
