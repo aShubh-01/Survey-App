9ab68bcd4a09b929dc7e2dd57b3d0836e47732f1
Ashubh01 <shubhamdhokare01@gmail.com>
2024-08-07 22:14:01 +0530

    Changed the layout of the table for published survey monitor

--- frontend/src/components/pages/Dashboard.jsx
+++ frontend/src/components/pages/Dashboard.jsx
@@ -11,7 +11,7 @@ export default function DashboardComponent() {
                     <div><ButtonComponent label="Saved Surveys" icon={<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="md:size-10 size-7"><path strokeLinecap="round" strokeLinejoin="round" d="M16.5 3.75V16.5L12 14.25 7.5 16.5V3.75m9 0H18A2.25 2.25 0 0 1 20.25 6v12A2.25 2.25 0 0 1 18 20.25H6A2.25 2.25 0 0 1 3.75 18V6A2.25 2.25 0 0 1 6 3.75h1.5m9 0h-9" /></svg> } /></div>
                 </div>
             </div>
-            <div className='m-[15px] flex justify-center'>
+            <div className='md:m-[50px] m-[15px] flex justify-center'>
                 <PublishedSurveysComponent />
             </div>
         </div>
@@ -28,7 +28,7 @@ const ButtonComponent = ({label, icon, onClickDo}) => {
     return <div className='flex justify-center md:rounded-[20px]
                 m-[5px] bg-slate-300 rounded-[10px]'>
         <button className='md:p-[40px] md:px-[50px] gap-[10px]
-                p-[10px] px-[70px] flex justify-center gap-[5px]' onClick={onClickDo}>
+                p-[10px] px-[75px] flex justify-center gap-[5px]' onClick={onClickDo}>
             <div>{icon}</div>
             <div className='md:p-[2px] md:text-[25px] md:font-bold
                 p-[1px] text-[18px] font-semibold'>{label}</div>
@@ -43,73 +43,41 @@ const PublishedSurveysComponent = () => {
     if(surveys.isLoading) return <div>Loading</div>
 
     console.log(surveys.surveys.publishedSurveys);
-    return <div>
-        <table className='w-[305px] border-2 border-black'>
+    return <div className='md:text-[21px]'>
+        <table className='md:w-[1000px] md:border-4
+            border-2 border-black'>
             <thead>
-                <tr className='border-2 border-black'>
-                    <th className='w-1/2 text-left p-1'>Title</th>
+                <tr>
+                    <th className='w-1/2 text-left p-1 pl-[12px]'>Title</th>
                     <th className='w-1/3 text-right p-1'>Responses</th>
                     <th className='w-1/3 text-right p-1'>Status</th>
                 </tr>
             </thead>
         </table>
-        <div className='my-[5px] border-black border-2 overflow-y-auto max-h-[180px]'>
+        <div className='md:max-h-[400px] md:h-[400px]
+            my-[5px] overflow-y-auto max-h-[180px]'>
             <table>
-            <tbody>
+                <tbody>
                     <tr>
-                        <td className='w-[140px] px-[5px] text-left'>Survey first</td>
-                        <td className='w-[105px] px-[5px] text-center'>4</td>
-                        <td className='w-[53px] text-left'>Closed</td>
+                        <div className='my-[3px] border-black border-2'>
+                            <td className='w-[150px] p-[7px] text-left'>Survey 1st</td>
+                            <td className='w-[100px] text-center'>4</td>
+                            <td className='w-right pr-[5px]'>Closed</td>
+                       </div>
                     </tr>
                     <tr>
-                        <td className='w-[140px] px-[5px] text-left'>Survey first</td>
-                        <td className='w-[105px] px-[5px] text-center'>4</td>
-                        <td className='w-[53px] text-left'>Closed</td>
+                        <div className='my-[3px] border-black border-2'>
+                            <td className='w-[150px] p-[7px] text-left'>Survey second</td>
+                            <td className='w-[100px] text-center'>11</td>
+                            <td className='w-right pr-[2px]'>Open</td>
+                       </div>
                     </tr>
                     <tr>
-                        <td className='w-[140px] px-[5px] text-left'>Survey first</td>
-                        <td className='w-[105px] px-[5px] text-center'>4</td>
-                        <td className='w-[53px] text-left'>Closed</td>
-                    </tr>
-                    <tr>
-                        <td className='w-[140px] px-[5px] text-left'>Survey first</td>
-                        <td className='w-[105px] px-[5px] text-center'>4</td>
-                        <td className='w-[53px] text-left'>Closed</td>
-                    </tr>
-                    <tr>
-                        <td className='w-[140px] px-[5px] text-left'>Survey first</td>
-                        <td className='w-[105px] px-[5px] text-center'>4</td>
-                        <td className='w-[53px] text-left'>Closed</td>
-                    </tr>
-                    <tr>
-                        <td className='w-[140px] px-[5px] text-left'>Survey first</td>
-                        <td className='w-[105px] px-[5px] text-center'>4</td>
-                        <td className='w-[53px] text-left'>Closed</td>
-                    </tr>
-                    <tr>
-                        <td className='w-[140px] px-[5px] text-left'>Survey first</td>
-                        <td className='w-[105px] px-[5px] text-center'>4</td>
-                        <td className='w-[53px] text-left'>Closed</td>
-                    </tr>
-                    <tr>
-                        <td className='w-[140px] px-[5px] text-left'>Survey first</td>
-                        <td className='w-[105px] px-[5px] text-center'>4</td>
-                        <td className='w-[53px] text-left'>Closed</td>
-                    </tr>
-                    <tr>
-                        <td className='w-[140px] px-[5px] text-left'>Survey first</td>
-                        <td className='w-[105px] px-[5px] text-center'>4</td>
-                        <td className='w-[53px] text-left'>Closed</td>
-                    </tr>
-                    <tr>
-                        <td className='w-[140px] px-[5px] text-left'>Survey first</td>
-                        <td className='w-[105px] px-[5px] text-center'>4</td>
-                        <td className='w-[53px] text-left'>Closed</td>
-                    </tr>
-                    <tr>
-                        <td className='w-[140px] px-[5px] text-left'>Survey first</td>
-                        <td className='w-[105px] px-[5px] text-center'>4</td>
-                        <td className='w-[53px] text-left'>Closed</td>
+                        <div className='my-[3px] border-black border-2'>
+                            <td className='w-[150px] p-[7px] text-left'>Survey third</td>
+                            <td className='w-[100px] text-center'>0</td>
+                            <td className='w-right pr-[2px]'>Closed</td>
+                       </div>
                     </tr>
                 </tbody>
             </table>
