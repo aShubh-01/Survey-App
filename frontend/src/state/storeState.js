50d891d08a1f0b92612d8e20b44e7def496927d1
Ashubh01 <shubhamdhokare01@gmail.com>
2024-08-09 19:37:16 +0530

    Integrated a loadable redux state for fetching and rendering published surveys

--- frontend/src/state/storeState.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import surveyReducers from './features/surveySlice';
-import axios from 'axios';
-
-export const store = configureStore({
-    reducer: surveyReducers
-})
